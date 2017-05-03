ea01fa5658532aa5bb54de478fb99bb35b453c56
gaborgit128 <ggg128@gmail.com>
2017-05-03 10:37:17 +0200

    refactor unit tests

--- app/src/test/java/com/example/mobsoft/mobsoft_lab3/TestComponent.java
+++ app/src/test/java/com/example/mobsoft/mobsoft_lab3/TestComponent.java
@@ -4,7 +4,6 @@ package com.example.mobsoft.mobsoft_lab3;
  * Created by Android on 2017. 05. 03..
  */
 
-
 import com.example.mobsoft.mobsoft_lab3.interactor.InteractorModule;
 import com.example.mobsoft.mobsoft_lab3.mock.MockNetworkModule;
 import com.example.mobsoft.mobsoft_lab3.repository.TestRepositoryModule;
@@ -13,7 +12,8 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 
+
 @Singleton
-@Component(modules = {MockNetworkModule.class, TestModule.class, InteractorModule.class, TestRepositoryModule.class})
+//@Component(modules = {MockNetworkModule.class, TestModule.class, InteractorModule.class, TestRepositoryModule.class})
 public interface TestComponent extends MobSoftApplicationComponent {
 }
--- app/src/test/java/com/example/mobsoft/mobsoft_lab3/TestHelper.java
+++ app/src/test/java/com/example/mobsoft/mobsoft_lab3/TestHelper.java
@@ -13,7 +13,7 @@ public class TestHelper {
     public static void setTestInjector() {
         ShadowLog.stream = System.out;
         MobSoftApplication application = (MobSoftApplication) RuntimeEnvironment.application;
-        //MobSoftApplicationComponent injector = DaggerTestComponent.builder().testModule(new TestModule(application.getApplicationContext())).build();
+      //  MobSoftApplicationComponent injector = DaggerTestComponent.builder().testModule(new TestModule(application.getApplicationContext())).build();
         //application.setInjector(injector);
     }
 }
