9cbdd915eafe668e3d007dc8324a5cdacd5f5b93
gaborgit128 <ggg128@gmail.com>
2017-05-11 18:39:04 +0200

    add tests

--- app/src/test/java/com/example/mobsoft/mobsoft_lab3/test/AddAdvertTest.java
+++ app/src/test/java/com/example/mobsoft/mobsoft_lab3/test/AddAdvertTest.java
@@ -1,15 +1,18 @@
 package com.example.mobsoft.mobsoft_lab3.test;
 
+/**
+ * Created by Android on 2017. 05. 03..
+ */
+
 import com.example.mobsoft.mobsoft_lab3.BuildConfig;
-import com.example.mobsoft.mobsoft_lab3.ui.login.LoginPresenter;
-import com.example.mobsoft.mobsoft_lab3.ui.login.LoginScreen;
+import com.example.mobsoft.mobsoft_lab3.ui.add.AddPresenter;
+import com.example.mobsoft.mobsoft_lab3.ui.add.AddScreen;
 import com.example.mobsoft.mobsoft_lab3.utils.RobolectricDaggerTestRunner;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.robolectric.annotation.Config;
 
 import static com.example.mobsoft.mobsoft_lab3.TestHelper.setTestInjector;
@@ -17,31 +20,29 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-/**
- * Created by Android on 2017. 05. 08..
- */
 @RunWith(RobolectricDaggerTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
-public class LoginTest {
-    private LoginPresenter loginPresenter;
+public class AddAdvertTest {
+
+    private AddPresenter addPresenter;
 
     @Before
     public void setup() throws Exception {
         setTestInjector();
-        loginPresenter = new LoginPresenter();
+        addPresenter = new AddPresenter();
     }
 
     @Test
-    public void testLogin() {
-        LoginScreen loginScreen = mock(LoginScreen.class);
-        loginPresenter.attachScreen(loginScreen);
-        loginPresenter.login("Gabor", "12345");
+    public void testAddAdvert() {
+        AddScreen mainScreen = mock(AddScreen.class);
+        addPresenter.attachScreen(mainScreen);
+        addPresenter.addAdvert("Autó", "Jó állapotú", 10000);
 
-        verify(loginScreen, times(1)).login();
+        verify(mainScreen, times(1)).advertAdded();
     }
 
     @After
     public void tearDown() {
-        loginPresenter.detachScreen();
+        addPresenter.detachScreen();
     }
 }
